97dd5f6542f11d088a98f25c0d9f7b069635a9ce
ChandraShekhar <240231026@hbtu.ac.in>
2025-12-31 01:33:49 +0530

    Implement Update Task Status API and  Routes

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -22,3 +22,54 @@ export const createTask = async (req, res) => {
     });
   }
 };
+
+export const assignTask = async (req, res) => {
+  const { taskId, assignedUser } = req.body;
+
+  try {
+    const task = await Task.findByIdAndUpdate(taskId, 
+        { assignedUser }, 
+        { new: true });
+
+    if (!task) {
+
+      return res.status(404).json({ 
+        message: 'Task not found' 
+    });
+    }
+    res.json(task);
+  }
+   catch (error) {
+    res.status(500).json({
+         message: 'Server error' 
+        });
+  }
+};
+
+export const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find().populate('assignedUser', 
+        'name email').populate('createdUser', 'name email');
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+export const updateStatus = async (req, res) => {
+  const { taskId, status } = req.body;
+
+  try {
+    const task = await Task.findByIdAndUpdate(taskId, { status }, { new: true });
+    if (!task) {
+      return res.status(404).json({ 
+        message: 'Task not found' 
+    });
+    }
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ 
+        message: 'Server error'
+     });
+  }
+};
--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createTask, assignTask, getTasks } from '../controllers/task.controller.js';
+import { createTask, assignTask, getTasks, updateStatus } from '../controllers/task.controller.js';
 import authMiddleware from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/create', authMiddleware, createTask);
-router.post('/assign', authMiddleware, assignTask);
+router.post('/', authMiddleware, createTask);
+router.patch('/:id', authMiddleware, updateStatus); 
+router.patch('/:id/assign', authMiddleware, assignTask);  
 router.get('/', authMiddleware, getTasks);
 
 export default router;
